ddfa89ba66e73a579ea9a4a405c22be6aa8200b4
zchristy <zchristy44@gmail.com>
2019-06-20 23:44:23 -0500

    Complete testing of all routes, and models

--- api/server.js
+++ api/server.js
@@ -7,13 +7,60 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+  res.status(200).json({ message: 'Hello from the backend!' });
 });
 
-server.get('/teams', (req, res) => {
+server.get('/api/teams', (req, res) => {
   teams.find()
     .then(teams => {
       res.status(200).json(teams);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.get('/api/teams/:id', (req, res) => {
+  const { id } = req.params
+
+  teams.findById(id)
+    .then(team => {
+      res.status(200).json(team);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post('/api/teams', (req, res) => {
+
+  teams.add(req.body)
+    .then(team => {
+      res.status(200).json(team);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.put('/api/teams/:id', (req, res) => {
+  const { id } = req.params
+
+  teams.update(id, req.body)
+    .then(team => {
+      res.status(200).json(team);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.delete('/api/teams/:id', (req, res) => {
+  const { id } = req.params
+
+  teams.remove(id)
+    .then(id => {
+      res.status(200).json({message: "Your team has been deleted"});
     })
     .catch(error => {
       res.status(500).json(error);
